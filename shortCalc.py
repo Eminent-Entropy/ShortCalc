f6002d5a81fecdacd242a00f603da9f2df87fe13
Aiden G <orca950@gmail.com>
2016-11-16 21:21:01 -0600

    added to github

---
+++ shortCalc.py
@@ -0,0 +1,12 @@
+while True:
+        num1 = int(input("type in first number: "))
+        num2 = int(input("type in second number: "))
+        opp = input("type in +, -, *, or /: ")
+        if opp == "+":
+                print(num1 + num2)
+        elif opp == "-":
+                print(num1 - num2)
+        elif opp == "*":
+                print(num1 * num2)
+        elif opp == "/":
+                print(num1 / num2)
